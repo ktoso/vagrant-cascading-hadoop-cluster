8c06b430c986815e2c528fcec196c5dc170d8052
Konrad Malawski <konrad.malawski@project13.pl>
2013-08-08 01:23:53 +0100

    should install all needed things for phash

--- modules/phash/manifests/init.pp
+++ modules/phash/manifests/init.pp
@@ -2,7 +2,7 @@ class phash {
 
   Package { ensure => "installed" }
 
-  $packs = [ "libphash0", "libphash0:i386", "libphash0-dev", "libphash0-dev:i386" ]
+  $packs = [ "g++", "libphash0", "libphash0:i386", "libphash0-dev", "libphash0-dev:i386" ]
 
   package { $packs: }
 
