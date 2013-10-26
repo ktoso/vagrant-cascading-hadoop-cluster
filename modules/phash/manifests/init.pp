3a5d0e22a0b47539b3afb12bc2134f46ce2323e8
Konrad Malawski <konrad.malawski@project13.pl>
2013-10-26 14:52:09 +0100

    minifix

--- modules/phash/manifests/init.pp
+++ modules/phash/manifests/init.pp
@@ -2,7 +2,8 @@ class phash {
 
   Package { ensure => "installed" }
 
-  $packs = [ "g++", "libphash0", "libphash0:i386", "libphash0-dev", "libphash0-dev:i386" ]
+  # $packs = [ "g++", "libphash0", "libphash0-dev" ]
+  $packs = [ "libphash0"  ]
 
   package { $packs: }
 
